56fcd57bbdf881b43dac34366a0dbb3cf3c70f70
T450 <T450@Mari.mshome.net>
2023-03-15 19:43:48 -0300

    Banco 1.1- Atualizado

--- src/controller/Banco.java
+++ src/controller/Banco.java
@@ -9,6 +9,7 @@ public class Banco extends Thread{
 	private Semaphore c;
 	int conta;
 	int valor;
+	int deposito;
 	public Banco(Semaphore b, Semaphore c) {
 		this.b=b;
 		this.c=c;
@@ -29,14 +30,17 @@ public class Banco extends Thread{
 			
 	}
 	private void Saldo() {
-		 valor = (int )((Math.random()*10 )+100);
+		 valor = (int )((Math.random()*100 )+100);
 		conta = (int)((Math.random ()*100000)+ 100000);
-		System.out.println("Saldo-----> Conta : " + conta + "saldo : "+ valor   );
+	deposito = (int)((Math.random()* 100)+ 1000);		
+		System.out.println(" Deposito -----> Conta : " + conta + "  Saldo : "+ valor +" -->Deposito : " + deposito  );
 		
 	}
 	private void Saque () {
-		System.out.println(" SAQUE ----> Conta : " + conta  + " Saldo : "+valor);
-		double saque= Double.parseDouble(JOptionPane.showInputDialog("Informe o valor para sacar---->saldo: " + valor ));
+		int valor1 = valor + deposito;
+		System.out.println(" SAQUE ----> Conta : " + conta  + " Saldo : "+valor1);
+		int saque = (int)((Math.random()*12)+ 100);
+		System.out.println(" Valor de saque --> "+ saque );
 		System.out.println(" ");
 	}
 }
--- src/view/BancoPrincipal.java
+++ src/view/BancoPrincipal.java
@@ -9,7 +9,7 @@ public class BancoPrincipal {
 	public static void main(String[] args) {
 		Semaphore b = new Semaphore (1);
 		Semaphore c= new Semaphore (1);
-		for ( int i =0; i< 10; i++) {
+		for ( int i =0; i< 20; i++) {
 			Thread BB= new Banco (b,c);
 			BB.start();
 			
